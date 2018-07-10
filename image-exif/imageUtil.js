721bb2d2feec0e2a5e73375bd8a1082b9ea1b939
Youkyoung Park <kyoungah@kyoungah.com>
2018-07-11 00:28:46 +0900

    이미지 정방향으로 출력하기 예제 작성 완료

--- image-exif/imageUtil.js
+++ image-exif/imageUtil.js
@@ -7,10 +7,28 @@ function getExif(file) {
     EXIF.getData(image, () => {
       const tags = EXIF.getAllTags(image);
 
+      // metadata 출력
+      console.log(tags);
+
       // 이미지 미리보기
       imageViewer.style.backgroundImage = `url(${e.target.result})`;
     });
   };
 
   reader.readAsDataURL(image);
+}
+
+function fixRotationOfFile(file) {
+  const image = file.files[0];
+  const fileType = image.type;
+
+  loadImage(
+    image,
+    img => {
+      const imageViewer = document.getElementById("imageViewer");
+
+      imageViewer.appendChild(img);
+    },
+    { maxWidth: 400, orientation: true }
+  );
 }
